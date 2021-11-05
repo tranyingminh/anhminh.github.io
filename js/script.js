d463688c65c49d0634c5c9285302fe6794464e04
tranyingminh <93786662+tranyingminh@users.noreply.github.com>
2021-11-06 00:36:01 +0700

    up

--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
+const menuBar = document.querySelector(".menu-bar")
+menuBar.addEventListener("click",function(){
+    menuBar.classList.toggle("active")
+    document.querySelector(".menu-items").classList.toggle("active")
+})
+const toP = document.querySelector(".top")
+window.addEventListener("scroll",function(){
+    const x =this.pageYOffset;
+    if(x>98){toP.classList.add("active")}
+    else{toP.classList.remove("active")}
+})
+const menuTitle = document.querySelector(".menu-title");
+menuTitle.addEventListener("click",function(x){
+    if(x.target.classList.contains("menu-button")){
+        const Target = x.target.getAttribute("data-title");
+        //console.log(Target)
+        menuTitle.querySelector(".active").classList.remove("active");
+        x.target.classList.add("active");
+        
+        const menuItem = document.querySelector(".menu");
+        menuItem.querySelector(".menu-item-content.active").classList.remove("active");
+        menuItem.querySelector(Target).classList.add("active")
+    }
+})
+
+
+/*-----------------------------js cuộn------------------*/
 let anchorlinks = document.querySelectorAll('a[href^="#"]')
  
 for (let item of anchorlinks) { // relitere 
@@ -12,3 +39,5 @@ for (let item of anchorlinks) { // relitere
         e.preventDefault()
     })
 }
+
+/*--------------button tang giam---------------*/
